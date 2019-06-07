279c7d6fe5998ffada24d7c25bd8f01a8e4e94e9
Dennis Wegereef <denniswegereef@gmail.com>
2019-06-07 17:19:29 +0200

    created cahtbubbles

---
+++ client/components/client/BigHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="big-header">
+    <h2 class="big-header__title">
+      <span class="bold">Hey {{name}}</span>
+      Welk probleem wil je melden?
+    </h2>
+
+    <div class="big-header__wave">
+      <div class="svg-container">
+        <svg viewBox="0 0 286.33 149.3">
+          <path
+            d="M286.33,0.28v91.53c-116.65,0-86.51,26.23-178.05,26.79C64.73,118.87,0,149.3,0,149.3L0,0L286.33,0.28z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'No name'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/css/config/main.scss';
+
+.big-header {
+  min-height: 20rem;
+  display: block;
+  // @include linear-gradient($color-primary);
+  padding: $padding-xl * 2 $padding-s 0 $padding-l;
+  &__wave {
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+
+    height: 300px;
+  }
+
+  &__title {
+    color: $color-white;
+    span {
+      display: block;
+    }
+  }
+  path {
+    fill: $color-primary;
+  }
+}
+</style>
---
+++ client/components/client/ChatBubble.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="chat-bubble" :class="'chat-bubble--' + context">
+    <div class="chat-bubble--bubble">
+      <p class="chat-bubble--text">
+        <slot></slot>
+      </p>
+    </div>
+    <span class="chat-bubble--time">{{time}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    time: {
+      type: String,
+      default: 'No time'
+    },
+    context: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/css/config/main.scss';
+
+.chat-bubble {
+  display: flex;
+  flex-direction: column;
+  &--bubble {
+    padding: $padding-m;
+    max-width: 80%;
+    margin-bottom: $margin-xs;
+    box-shadow: $shadow-m;
+  }
+  &--text {
+    margin: 0;
+  }
+  &--time {
+    margin-bottom: $padding-l;
+    font-size: 0.9rem;
+  }
+  &--operator {
+    .chat-bubble {
+      &--bubble {
+        border-radius: $rounding-s $rounding-s $rounding-s 0;
+        background-color: $color-white;
+      }
+      &--text {
+        color: $color-grey-dark;
+      }
+    }
+  }
+  &--user {
+    .chat-bubble {
+      &--bubble {
+        border-radius: $rounding-s $rounding-s 0 $rounding-s;
+        align-self: flex-end;
+        @include linear-gradient($color-primary);
+      }
+      &--text {
+        color: $color-white;
+      }
+      &--time {
+        align-self: flex-end;
+      }
+    }
+  }
+}
+</style>
