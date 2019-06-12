9db9e7a996920404b94e15563ea6750f06bb581a
Dennis Wegereef <denniswegereef@gmail.com>
2019-06-12 13:12:34 +0200

    Working prototype flow of the client

---
+++ client/layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <img src="~/static/car_error.svg">
+    <h1 v-if="error.statusCode === 404">Pagina niet gevonden</h1>
+    <h1 v-else>Er is een fout opgetreden</h1>
+    <nuxt-link to="/">Terug naar home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/css/config/main.scss';
+
+img {
+  max-width: 20rem;
+  margin-bottom: $margin-m;
+}
+</style>
