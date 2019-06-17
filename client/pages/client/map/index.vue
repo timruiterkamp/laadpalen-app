7da097ed6b768165438e61fb13492567dbc74503
Dennis Wegereef <denniswegereef@gmail.com>
2019-06-17 14:46:30 +0200

    Moved auth to middleware

---
+++ client/pages/client/map/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Atlas height="calc(100vh - 5rem)"/>
+  </div>
+</template>
+
+<script>
+import Atlas from '~/components/shared/Atlas.vue'
+
+export default {
+  layout: 'client',
+  components: {
+    Atlas
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
---
+++ client/pages/client/notifications/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1></h1>
+    {{data}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  layout: 'client',
+  data() {
+    return {
+      data: []
+    }
+  },
+  mounted() {
+    console.log(this.$store.state.authToken)
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
+
