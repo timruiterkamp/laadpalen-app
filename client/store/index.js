1fa6e00d8555f902c4f0a254c277e31895ce0723
FollyWolly <folkertjanvanderpol@gmail.com>
2019-06-21 12:14:23 +0200

    Hotfix store

--- client/store/index.js
+++ client/store/index.js
@@ -34,65 +34,67 @@ export const mutations = {
 }
 
 export const actions = {
-  FETCH_LOADINGSTATION_DATA({ commit, state }) {
-    return DB.execute(
-      `query {
-        loadingstations {
-          _id
-          city
-          longitude
-          latitude
-          address
-          postalcode
-          provider
-          sockets
-          usedsockets
-          status
-          issues {
-            _id
-          }
-        }
-      }`,
-      state.authToken
-    )
-      .then(res => res.loadingstations.slice(0, 200))
-      .then(res => {
-        return res.map(station => {
-          const issueIds = station.issues
-          if (issueIds && issueIds.length > 0) {
-            return new Promise((resolve, reject) => {
-              const promises = issueIds.map(issue => {
-                return DB.execute(
-                  `
-                query {
-                  issue(id: "${issue._id}") {
-                    _id
-                    status
-                    location
-                    title
-                  }
-                }
-              `, state.authToken)
-              .then(data => {
-                if (!data) {
-                  console.error('No issue with id: ', issue._id, ' was found at loadingstation: ', station._id)
-                }
-                return data.issue
-              })
-              .catch(err => reject(err))
-            })
-            Promise.all(promises)
-              .then(issues => {
-                station.issues = issues
-                resolve(station)
-              })
-              .catch(err => reject(err))
-          })
-        }
-        return new Promise(resolve => resolve(station))
-      })
-      .catch(console.error)
-  }
+  FETCH_LOADINGSTATION_DATA({commit, state}) {
+   return DB.execute(
+     `query {
+       loadingstations {
+         _id
+         city
+         longitude
+         latitude
+         address
+         postalcode
+         provider
+         sockets
+         usedsockets
+         status
+         issues {
+           _id
+         }
+       }
+     }`, state.authToken)
+   .then(res => res.loadingstations.slice(0, 200))
+   .then(res => {
+     return res.map(station => {
+       const issueIds = station.issues
+       if (issueIds && issueIds.length > 0) {
+         return new Promise((resolve, reject) => {
+           const promises = issueIds.map(issue => {
+             return DB.execute(`
+               query {
+                 issue(id: "${issue._id}") {
+                   _id
+                   status
+                   location
+                   title
+                 }
+               }
+             `, state.authToken)
+             .then(data => {
+               if (!data) {
+                 console.error('No issue with id: ', issue._id, ' was found at loadingstation: ', station._id)
+               }
+               return data.issue
+             })
+             .catch(err => reject(err))
+           })
+           Promise.all(promises)
+             .then(issues => {
+               station.issues = issues
+               resolve(station)
+             })
+             .catch(err => reject(err))
+         })
+       }
+       return new Promise(resolve => resolve(station))
+     })
+   })
+   .then(res => {
+     return Promise.all(res)
+       .then(res => commit('SET_LOADINGSTATION_DATA', res))
+   })
+   .catch(console.error)
+ }
 }
 
 export const state = () => ({
